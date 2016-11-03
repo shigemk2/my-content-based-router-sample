29a71b2440c248ed56442f19eab1c18169bb81f6
shigemk2 <i.am.shige@gmail.com>
2016-11-03 17:05:45 +0900

    Add case classes

--- src/main/scala/com/example/ContentBasedRouter.scala
+++ src/main/scala/com/example/ContentBasedRouter.scala
@@ -2,5 +2,19 @@ package com.example
 
 import akka.actor._
 
-object ContentBasedRouterDriver {
+case class Order(id: String, orderType: String, orderItems: Map[String, OrderItem]) {
+  val grandTotal: Double = orderItems.values.map(orderItem => orderItem.price).sum
+
+  override def toString: String = {
+    s"Order($id, $orderType, $orderItems, Totaling: $grandTotal)"
+  }
+}
+
+case class OrderItem(id: String, itemType: String, description: String, price: Double) {
+  override def toString: String = {
+    s"OrderItem($id, $itemType, '$description', $price)"
+  }
+}
+
+object ContentBasedRouterDriver extends CompletableApp(3) {
 }
